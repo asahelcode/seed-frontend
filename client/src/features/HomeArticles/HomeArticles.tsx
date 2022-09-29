961eaeef8b1e5ac62a571968c960f98d064a2af9
asahelcode <hoyx0101@gmail.com>
2022-09-29 22:22:05 +0000

    Add Article container for home page with styles

--- client/src/features/HomeArticles/HomeArticles.tsx
+++ client/src/features/HomeArticles/HomeArticles.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Container } from '@mui/material'
+import Article from '../Article/Article';
 
 import articles from '../../data.json';
 
+import useStyle from './styles'
 
 const HomeArticles = () => {
+  const classes = useStyle()
+
   return (
-    <Container maxWidth='md'>
-      
+    <Container maxWidth='md' className={classes.container}>
+      {
+        articles.map((item) => <Article key={item.id} article={item} />)
+      }
     </Container>
   )
 }
