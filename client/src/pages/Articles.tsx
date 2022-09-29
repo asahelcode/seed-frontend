71b91b8ace1f5775846385ae476f81c1902828a2
asahelcode <hoyx0101@gmail.com>
2022-09-29 22:24:06 +0000

    Add Home and Article page

--- client/src/pages/Articles.tsx
+++ client/src/pages/Articles.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
+
 const Articles = () => {
   return (
-    <div>Articles</div>
+    <div>Hello</div>
   )
 }
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Header from '../features/Header/Header'
 import Hero from '../features/Hero/Hero'
 import HomeArticles from '../features/HomeArticles/HomeArticles'
 
 const Home = () => {
   return (
     <>
-      <Header />
       <Hero />
       <HomeArticles />
     </>
