6ffe4dbe8e2ce8a6e03b467005af239e709f1259
asahelcode <hoyx0101@gmail.com>
2022-09-30 15:09:05 +0000

    Add Article detail component with add styles

--- client/src/styles.tsx
+++ client/src/styles.tsx
@@ -2,6 +2,25 @@ import { makeStyles } from "@material-ui/styles";
 
 export default makeStyles((theme) => ({
   body: {
-    background: '#fff'
+    marginTop: '0.9em',
+    backgroundColor: '#fff',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   },
+  projectBody: {
+    backgroundColor: 'rgba(255, 255, 255, 0.79)',
+  },
+  articleOptionLink: {
+    color: '#000',
+    textDecoration: 'none',
+    "&:hover": {
+      color: 'blue',
+    }
+  },
+  articleDetailBody: {
+    marginTop: '2.3em',
+    backgroundColor: 'rgba(255, 255, 255, 0.73)'
+  }
 }))
