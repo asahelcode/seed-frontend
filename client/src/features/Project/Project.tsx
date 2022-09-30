4609bd945889af77aedb8d199c5dcaef7703e9c5
asahelcode <hoyx0101@gmail.com>
2022-09-30 12:01:22 +0000

    Finish project component

--- client/src/features/Project/Project.tsx
+++ client/src/features/Project/Project.tsx
@@ -1,17 +1,47 @@
 import { Container } from '@material-ui/core'
-import { Stack, Box } from '@mui/material'
+import { Stack, Typography, Button } from '@mui/material'
 import React from 'react'
 import useStyles from './styles'
 
 
 import { ProjectProp } from '../../interfaces'
+import { Link } from 'react-router-dom'
 
 const Project = ({ project }: ProjectProp) => {
   const classes = useStyles();
   return (
-    <Container maxWidth='md'>
-      <Stack>
+    <Container maxWidth='md' className={classes.body}>
+      <Stack direction='row' justifyContent='center' mt={3} position='r7elative'>
         <img src={project.img} alt="" className={classes.projectImg} />
+        <Stack position='absolute' backgroundColor='rgba(0, 0, 0, 0.38)' color='#fff' p={1}>
+          <Typography textTransform='capitalize' variant='h6'>{project.name}</Typography>
+        </Stack>
+      </Stack>
+      <Stack direction='row' justifyContent='center' alignItems='center' mt={2} gap={8}>
+        <Button variant='outlined'>
+          <Typography textTransform='capitalize'>
+            <a href='https://github.com/asahelcode' target="_blank" rel='noreferrer' style={{ textDecoration: 'none' }}>
+              Github repo
+            </a>
+          </Typography>
+        </Button>
+        <Button variant='contained'>
+          <Typography textAlign='center' textTransform='capitalize'><a href={project.url} target="_blank" rel="noreferrer" style={{ textDecoration: 'none', color: '#fff' }}>demo</a></Typography>
+        </Button>
+      </Stack>
+      <Stack direction='row' justifyContent='center' mt={3}>
+        <Typography width='40ch' lineHeight='25px'>
+          {project.description}
+        </Typography>
+      </Stack>
+
+      <Stack direction='row' flexWrap='wrap' gap={1} mb={4} mt={1} justifyContent='center'>
+        {
+          project.technologies.map((technology) => <Button disabled variant='contained'><Typography textTransform='capitalize'>{technology}</Typography></Button>)
+        }
+      </Stack>
+      <Stack direction='row' justifyContent='center'>
+        <hr className={classes.bisector} />
       </Stack>
     </Container>
   )
--- client/src/features/Project/styles.tsx
+++ client/src/features/Project/styles.tsx
@@ -1,8 +1,19 @@
 import { makeStyles } from "@material-ui/styles";
 
 export default makeStyles((theme) => ({
-  projectImg: {
-    width: '100%',
-    height: '200px',
+  body: {
+    zIndex: -1,
   },
+  projectImg: {
+    width: '300px',
+    height: '250px',
+    objectFit: 'cover',
+    boxShadow: '1px 2px 2px rgba(0, 0, 0, 0.19)',
+  },
+  bisector: {
+    border: 'none',
+    height: '5px',
+    width: '430px',
+    boxShadow: '1px 2px 0.9px rgba(0, 0, 0, 0.18)'
+  }
 }))
