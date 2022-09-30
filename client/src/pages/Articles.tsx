e8eaa709c48967003aeb121d8e0ef12c0c69dc7a
asahelcode <hoyx0101@gmail.com>
2022-09-30 12:02:28 +0000

    Complete Projects with Articles page

--- client/src/pages/Articles.tsx
+++ client/src/pages/Articles.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import { Container } from '@material-ui/core';
+import { Stack, Typography } from '@mui/material';
+
+import articles from '../data.json';
+import Article from '../features/Article/Article';
+import useStyles from '../styles'
 
 
 const Articles = () => {
+  const { category } = useParams();
+  const classes = useStyles();
+
   return (
-    <div>Hello</div>
+    <Container className={classes.body}>
+      <Typography variant='h5' textAlign='center' textTransform='capitalize' mt={4}>{category}</Typography>
+      {
+        articles.map((item) => <Article key={item.id} article={item} />)
+      }
+    </Container>
   )
 }
 
--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,8 +1,19 @@
+import { Container } from '@material-ui/core'
+import { Stack } from '@mui/material'
 import React from 'react'
+import Project from '../features/Project/Project'
+
+import projects from '../projects.json'
+import useStyle from '../styles'
 
 const Projects = () => {
+  const classes = useStyle()
   return (
-    <div>Projects</div>
+    <Container className={classes.projectBody}>
+      {
+        projects.map((project) => <Project key={project.id} project={project} />)
+      }
+    </Container>
   )
 }
 
