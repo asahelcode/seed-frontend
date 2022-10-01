966596a2f36c3bff898544ce6b0daee3e30fc34f
asahelcode <hoyx0101@gmail.com>
2022-10-01 15:15:26 +0000

    Update Blog and BlogDetail page

--- client/src/pages/BlogDetail.tsx
+++ client/src/pages/BlogDetail.tsx
@@ -4,43 +4,46 @@ import { useParams } from 'react-router-dom'
 import { Stack, Typography } from '@mui/material'
 import TwitterIcon from '@material-ui/icons/Twitter'
 
+import { format } from 'timeago.js'
+import parser from 'html-react-parser'
+
 import useStyle from '../styles'
 
-import articles from '../data.json';
+import blogs from '../data.json';
 
-const ArticleDetail = () => {
+const BlogDetail = () => {
   const { id } = useParams();
-  const article = articles[0];
+  const blog = blogs[0];
 
   const classes = useStyle();
 
 
   return (
-    <Container maxWidth='md' className={classes.articleDetailBody}>
+    <Container maxWidth='md' className={classes.blogDetailBody}>
       <Stack direction='row' justifyContent='center'>
-        <Typography sx={{ fontSize: { lg: '3em', xs: '1.5em' } }}>{article.title}</Typography>
+        <Typography sx={{ fontSize: { lg: '3em', xs: '1.5em' } }}>{blog.title}</Typography>
       </Stack>
       <Stack direction='row' alignItems='center' justifyContent='space-between' mt={2} borderBottom='1px solid gray' pb={2}>
         <Stack>
           <Typography fontSize='0.75em' color='rgba(0, 0, 10, 0.9)'>
-            {article.duration}
+            {format(blog.timestamp)}
           </Typography>
         </Stack>
         <Stack direction='row' gap={0.6}>
           {
-            article.tags.map((tag) => <Typography fontSize='0.75em' color='rgba(0, 0, 10, 0.8)'>{`#${tag}`}</Typography>)
+            blog.tags.map((tag) => <Typography fontSize='0.75em' color='rgba(0, 0, 10, 0.8)'>{`#${tag}`}</Typography>)
           }
         </Stack>
       </Stack>
       <Stack mt={2}>
-        <Typography fontFamily='Bellota' color='#000' variant='h6'>{article.description}</Typography>
+        <Typography fontFamily='Bellota' color='#000' variant='h6'>{parser(blog.description)}</Typography>
       </Stack>
       <Stack direction='row' justifyContent='flex-end' mt={3} alignItems='center' gap={1}>
-        <a href={`http://twitter.com/share?text=${article.title + ': ' + article.description.substring(0, 80) + '...'}:&url=${window.location.href}&hashtags=#tech,#revolution`} rel='noreferrer' target='_blank'>Tweet</a>
-        <TwitterIcon fontSize='small'/>
+        <a href={`http://twitter.com/share?text=${blog.title + ': ' + blog.description.substring(0, 80) + '...'}:&url=${window.location.href}&hashtags=#tech,#revolution`} rel='noreferrer' target='_blank'>Tweet</a>
+        <TwitterIcon fontSize='small' />
       </Stack>
     </Container >
   )
 }
 
-export default ArticleDetail
+export default BlogDetail
--- client/src/pages/Blogs.tsx
+++ client/src/pages/Blogs.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Link, useParams } from 'react-router-dom';
 import { Container } from '@material-ui/core';
-import { Stack, Typography } from '@mui/material';
+import {  Typography } from '@mui/material';
 
 import blogs from '../data.json';
 import Blog from '../features/Blog/Blog';
@@ -9,20 +8,13 @@ import useStyles from '../styles'
 
 
 const Articles = () => {
-  const { category } = useParams();
   const classes = useStyles();
 
   return (
     <Container className={classes.body}>
-      <Stack direction='row' gap={4} mt={3} sx={{ display: { lg: 'flex', xs: 'none' } }}>
-        <Link to='/books' className={classes.articleOptionLink}>
-          <Typography variant='h6'>Books</Typography>
-        </Link>
-        <Link to='/projects' className={classes.articleOptionLink}>
-          <Typography variant='h6'>Projects</Typography>
-        </Link>
-      </Stack>
-      <Typography variant='h4' textAlign='center' textTransform='capitalize' mt={4}>{category}</Typography>
+      <Typography variant='h5' color='lightslategrey' textAlign='center' fontFamily='Festive' textTransform='capitalize' mt={4}>
+        "Because the one who are crazy enough to think that<br /> they can change the world are the ones who do."
+      </Typography>
       {
         blogs.map((item) => <Blog key={item.id} blog={item} />)
       }
