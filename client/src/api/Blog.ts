4972216ca3ad76d8d357f03be72ba868d86b0f79
asahelcode <hoyx0101@gmail.com>
2022-10-03 04:12:30 -0400

    Add axios request for blogs

--- client/src/api/Blog.ts
+++ client/src/api/Blog.ts
@@ -1,10 +1,10 @@
-import { BlogProp } from './../interfaces';
+import {  ReqBlog } from './../interfaces';
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:5000';
+const baseUrl = 'http://localhost:5000/blog';
 
 export const getAllBlogs = () => axios.get(baseUrl);
 
-export const createBlog = (data: BlogProp) => axios.post(baseUrl, data);
+export const createBlog = (data: ReqBlog) => axios.post(baseUrl, data);
 
 export const getBlogById = (id: number) => axios.get(`${baseUrl}/${id}`);
