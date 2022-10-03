71d9122ca19c9d345709649ffc72c6942c762b51
asahelcode <hoyx0101@gmail.com>
2022-10-03 09:29:57 -0400

    Add book reducer

---
+++ client/src/reducer/Book.ts
@@ -0,0 +1,15 @@
+// import { bookDispatchType } from './../action/Book';
+// import { Book } from '../action/Blog';
+
+// const reducer = (books: any = [], action: bookDispatchType) => {
+//   switch (action.type) {
+//     case 'FETCH_ALL_BOOKS':
+//       return action.payload;
+//     case 'CREATE_BOOK':
+//       return action.payload;
+//     default:
+//       return books;
+//   }
+// };
+
+// export default reducer;
