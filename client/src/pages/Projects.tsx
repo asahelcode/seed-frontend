91cbc2dcef509aa61475ceb553b33219a65fcb78
asahelcode <hoyx0101@gmail.com>
2022-10-03 12:06:31 -0400

    Fetch api using redux and action creators

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,13 +1,19 @@
 import { Container } from '@material-ui/core'
-import { Stack } from '@mui/material'
 import React from 'react'
 import Project from '../features/Project/Project'
 
 import projects from '../projects.json'
 import useStyle from '../styles'
+import { RootState } from '../store/store'
+import { useDispatch, useSelector } from 'react-redux';
+
 
 const Projects = () => {
   const classes = useStyle()
+  const dispatch = useDispatch();
+  const projects = useSelector((state: RootState) => state.bookReducer)
+
+  
   return (
     <Container className={classes.projectBody}>
       {
