d43189a891c4fcaf8d440ca055c78aefb6092d78
asahelcode <hoyx0101@gmail.com>
2022-10-03 04:13:16 -0400

    Add interface for blog

--- client/src/interfaces.tsx
+++ client/src/interfaces.tsx
@@ -1,6 +1,13 @@
-
-export interface HomeArticlesProp {
-  article: { id: string, duration: string, category: string, title: string, tags: string[], description: string }
+export type ReqBlog = {
+  title: string,
+  tags: string[],
+  description: string,
+}
+export interface BlogProp {
+  blog: { id: string, timestamp: string, title: string, tags: string[], description: string }
+}
+export interface BookProp {
+  book: { id: string, timestamp: string, cover: string, genre: string, author: string, publisher: string, rating: number, review: string, url: string, pages: number, name: string }
 }
 
 export interface ProjectProp {
