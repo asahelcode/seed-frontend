3fd517ecb8e4f9251418743047c88b142f94b247
Henry Nnamani <hoyx0101@gmail.com>
2022-10-04 09:51:01 +0000

    Delete client directory

--- client/src/api/Project.ts
+++
@@ -1,9 +0,0 @@
-import { ReqProject } from './../interfaces';
-import axios from 'axios';
-
-const baseUrl = 'http://localhost:5000/project';
-
-export const getProjects = () => axios.get(baseUrl);
-
-export const createProject = (project: ReqProject) =>
-  axios.post(baseUrl, project);
--- client/src/reducer/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import blogReducer from './Blog';
-import bookReducer from './Book';
-import projectReducer from './Project';
-
-export default combineReducers({ blogReducer, bookReducer, projectReducer });
